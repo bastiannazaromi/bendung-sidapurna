19e9e878ceefa7120aff4332536cdd44becdb9c8
bastiannazaromi <57742252+bastiannazaromi@users.noreply.github.com>
2020-04-26 15:55:24 +0700

    Update footer.php

--- application/views/frontend/footer.php
+++ application/views/frontend/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h4><center><a style="color: black" href="http://sidapurnabendung.com" target="_blank" tittle="Bendung Sidapurna">&copy; 2019 sidapurnabendung.com</a></center></h4>
+                <h4><center><a style="color: black" href="http://sidapurna.duniaiot.com" target="_blank" tittle="Bendung Sidapurna">&copy; 2019 sidapurnabendung.com</a></center></h4>
             </div>
 
         </div>
